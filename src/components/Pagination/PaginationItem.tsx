f58c773726fc664820a526450332c4112c88bcb7
kameikay <kameikay@gmail.com>
2021-10-19 23:44:45 -0300

    pagination almost done

--- src/components/Pagination/PaginationItem.tsx
+++ src/components/Pagination/PaginationItem.tsx
@@ -3,11 +3,13 @@ import { Button } from "@chakra-ui/react";
 interface PaginationItemProps {
   pageNumber: number;
   isCurrent?: boolean;
+  onPageChange: (page:number) => void
 }
 
 export function PaginationItem({
   isCurrent = false,
   pageNumber,
+  onPageChange
 }: PaginationItemProps) {
   if (isCurrent) {
     return (
@@ -31,6 +33,7 @@ export function PaginationItem({
       width="4"
       bg="gray.700"
       _disable={{ bgColor: "gray.500" }}
+      onClick={() => onPageChange(pageNumber)}
     >
       {pageNumber}
     </Button>
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Link from "next/link";
 
 import {
@@ -15,17 +16,24 @@ import {
   Td,
   Tbody,
   useBreakpointValue,
+  Spinner,
 } from "@chakra-ui/react";
-import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { RiAddLine } from "react-icons/ri";
+
 import Header from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
+import { useUsers } from "../../services/hooks/useUsers";
 
 export default function UserList() {
+  const [page, setPage] = useState(1)
+  const { data, isLoading, isFetching, error } = useUsers(page)
+
   const isWideScreen = useBreakpointValue({
     base: false,
     lg: true,
   });
+
   return (
     <Box>
       <Header />
@@ -37,6 +45,9 @@ export default function UserList() {
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">
               Usuários
+              {!isLoading && isFetching && (
+                <Spinner size="sm" color="gray.500" ml="4" />
+              )}
             </Heading>
 
             <Link href="/users/create" passHref>
@@ -52,75 +63,59 @@ export default function UserList() {
             </Link>
           </Flex>
 
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px={["4", "4", "6"]} color="gray.300" width="8">
-                  <Checkbox colorScheme="pink" />
-                </Th>
+          {isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justify="center">
+              <Text>Falha ao obter dados.</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme="whiteAlpha">
+                <Thead>
+                  <Tr>
+                    <Th px={["4", "4", "6"]} color="gray.300" width="8">
+                      <Checkbox colorScheme="pink" />
+                    </Th>
 
-                <Th>Usuário</Th>
+                    <Th>Usuário</Th>
 
-                {isWideScreen && <Th>Data de cadastro</Th>}
-                <Th width="6"></Th>
-              </Tr>
-            </Thead>
+                    {isWideScreen && <Th>Data de cadastro</Th>}
+                    <Th width="6"></Th>
+                  </Tr>
+                </Thead>
 
-            <Tbody>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Victor Kamei Kay</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      kameikay@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
+                <Tbody>
+                  {data.users.map((user) => (
+                    <Tr key={user.id}>
+                      <Td px={["4", "4", "6"]}>
+                        <Checkbox colorScheme="pink" />
+                      </Td>
+                      <Td>
+                        <Box>
+                          <Text fontWeight="bold">{user.name}</Text>
+                          <Text fontSize="sm" color="gray.300">
+                            {user.email}
+                          </Text>
+                        </Box>
+                      </Td>
 
-                {isWideScreen && <Td>07 de Outubro, 2021</Td>}
-                <Td></Td>
-              </Tr>
+                      {isWideScreen && <Td>{user.createdAt}</Td>}
+                      <Td></Td>
+                    </Tr>
+                  ))}
+                </Tbody>
+              </Table>
 
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Victor Kamei Kay</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      kameikay@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-
-                {isWideScreen && <Td>07 de Outubro, 2021</Td>}
-                <Td></Td>
-              </Tr>
-
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Victor Kamei Kay</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      kameikay@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-
-                {isWideScreen && <Td>07 de Outubro, 2021</Td>}
-                <Td></Td>
-              </Tr>
-            </Tbody>
-          </Table>
-
-          <Pagination />
+              <Pagination 
+                totalCountOfRegisters={data.totalCount}
+                currentPage={page}
+                onPageChange={setPage}
+              />
+            </>
+          )}
         </Box>
       </Flex>
     </Box>
